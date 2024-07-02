6a655061782b758d2469a757d25ffed7ccdf92bc
ADMIN <ADMIN@DESKTOP-BENBA9P>
2024-07-02 18:27:23 +0530

    New line added

--- src/com/git/learn/GitLearn.java
+++ src/com/git/learn/GitLearn.java
@@ -4,8 +4,10 @@ public class GitLearn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("This Project is for Git Learning");
+
+		System.out.println("Checking new line added in git");
 
 	}
 
