7ff6197ef7a1acacfc0112ac14849793efcc36e7
ADMIN <ADMIN@DESKTOP-BENBA9P>
2024-07-02 17:43:22 +0530

    Initial commit

---
+++ src/com/git/learn/GitLearn.java
@@ -0,0 +1,12 @@
+package com.git.learn;
+
+public class GitLearn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This Project is for Git Learning");
+
+	}
+
+}
